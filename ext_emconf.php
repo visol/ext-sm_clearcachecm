4671f1b8046f65520392c1fa74d06e7030a1d202
xonx <xonx@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-30 12:22:33 +0000

    Uploaded as version 2.0.0 to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sm_clearcachecm/trunk@45962 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sm_clearcachecm".
 #
-# Auto generated 29-03-2011 16:04
+# Auto generated 30-03-2011 00:29
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,7 +11,7 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Clear cache clickmenu item',
+	'title' => 'Clear cache clickmenu items',
 	'description' => 'Adds items to the pagetree clickmenu to clear the cache of pages. Either clears cache of a single page or a whole branch. Two clicks are enough! Needs TYPO3 4.5 or later.',
 	'category' => 'be',
 	'author' => 'Steffen Mueller',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-0.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"1924";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"7bff";s:14:"ext_tables.php";s:4:"ac41";s:33:"Classes/Hooks/ClickmenuAction.php";s:4:"168d";s:59:"Ressources/Private/JavaScript/ClearCacheClickmenuActions.js";s:4:"0206";s:44:"Ressources/Private/Language/locallang_cm.xml";s:4:"578d";s:62:"Ressources/Private/Php/RegisterJavaScriptForPagetreeAction.php";s:4:"f991";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"1924";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"7bff";s:14:"ext_tables.php";s:4:"ac41";s:33:"Classes/Hooks/ClickmenuAction.php";s:4:"788a";s:44:"Ressources/Private/Language/locallang_cm.xml";s:4:"578d";s:62:"Ressources/Private/Php/RegisterJavaScriptForPagetreeAction.php";s:4:"faf1";s:58:"Ressources/Public/JavaScript/ClearCacheClickmenuActions.js";s:4:"2466";s:14:"doc/manual.sxw";s:4:"cbfa";}',
 	'suggests' => array(
 	),
 );
